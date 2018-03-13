fa7134631ef38eceddd86a54040cfdfcdd37e3fa
PS Perkins <psperkins@gmail.com>
2018-03-13 08:56:55 -0600

    update redirection > 3.2

--- wp-content/plugins/redirection/matches/http-header.php
+++ wp-content/plugins/redirection/matches/http-header.php
@@ -1,19 +1,21 @@
 <?php
 
-class Agent_Match extends Red_Match {
-	public $agent;
+class Header_Match extends Red_Match {
+	public $name;
+	public $value;
 	public $regex;
 	public $url_from;
 	public $url_notfrom;
 
 	function name() {
-		return __( 'URL and user agent', 'redirection' );
+		return __( 'URL and HTTP header', 'redirection' );
 	}
 
 	public function save( array $details, $no_target_url = false ) {
 		$data = array(
 			'regex' => isset( $details['regex'] ) && $details['regex'] ? true : false,
-			'agent' => isset( $details['agent'] ) ? $this->sanitize_agent( $details['agent'] ) : '',
+			'name'  => isset( $details['name'] ) ? $this->sanitize_name( $details['name'] ): '',
+			'value' => isset( $details['value'] ) ? $this->sanitize_value( $details['value'] ) : '',
 		);
 
 		if ( $no_target_url === false ) {
@@ -24,21 +26,30 @@ class Agent_Match extends Red_Match {
 		return $data;
 	}
 
-	private function sanitize_agent( $agent ) {
-		return $this->sanitize_url( $agent );
+	public function sanitize_name( $name ) {
+		$name = $this->sanitize_url( $name );
+		$name = str_replace( ' ', '', $name );
+		$name = preg_replace( '/[^A-Za-z0-9\-_]/', '', $name );
+
+		return trim( trim( $name, ':' ) );
+	}
+
+	public function sanitize_value( $value ) {
+		return $this->sanitize_url( $value );
 	}
 
 	function get_target( $url, $matched_url, $regex ) {
-		// Check if referrer matches
-		$matched = $this->agent === Redirection_Request::get_user_agent();
+		$target = false;
+		$matched = Redirection_Request::get_header( $this->name ) === $this->value;
+
 		if ( $this->regex ) {
-			$matched = preg_match( '@'.str_replace( '@', '\\@', $this->agent ).'@i', Redirection_Request::get_user_agent() ) > 0;
+			$matched = preg_match( '@'.str_replace( '@', '\\@', $this->value ).'@', Redirection_Request::get_header( $this->name ), $matches ) > 0;
 		}
 
-		$target = false;
-		if ( $this->url_from !== '' && $matched ) {
+		// Check if referrer matches
+		if ( $matched && $this->url_from !== '' ) {
 			$target = $this->url_from;
-		} elseif ( $this->url_notfrom !== '' && ! $matched ) {
+		} elseif ( ! $matched && $this->url_notfrom !== '' ) {
 			$target = $this->url_notfrom;
 		}
 
@@ -54,7 +65,8 @@ class Agent_Match extends Red_Match {
 			'url_from' => $this->url_from,
 			'url_notfrom' => $this->url_notfrom,
 			'regex' => $this->regex,
-			'agent' => $this->agent,
+			'name' => $this->name,
+			'value' => $this->value,
 		);
 	}
 
@@ -63,10 +75,14 @@ class Agent_Match extends Red_Match {
 
 		if ( isset( $values['url_from'] ) ) {
 			$this->url_from = $values['url_from'];
+		}
+
+		if ( isset( $values['url_notfrom'] ) ) {
 			$this->url_notfrom = $values['url_notfrom'];
 		}
 
 		$this->regex = $values['regex'];
-		$this->agent = $values['agent'];
+		$this->name = $values['name'];
+		$this->value = $values['value'];
 	}
 }
